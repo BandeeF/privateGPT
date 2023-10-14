1402a16fa3913f83376face2bedc032a5c4b12de
BandeeF <59371379+BandeeF@users.noreply.github.com>
2023-10-14 20:21:59 +0200

    -vicuna -MODEL_CTX_W -jenkins docs -llama.cpp 0.1.78 -n_gpu_layers

--- privateGPT.py
+++ privateGPT.py
@@ -20,10 +20,12 @@ persist_directory = os.environ.get('PERSIST_DIRECTORY')
 model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
+model_ctx_w = os.environ.get('MODEL_CTX_W')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
 from constants import CHROMA_SETTINGS
+
 
 def main():
     # Parse the command line arguments
@@ -37,7 +39,7 @@ def main():
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_ctx=model_ctx_w, n_batch=model_n_batch, callbacks=callbacks, verbose=False, n_gpu_layers=10)  #, n_threads=14)
         case "GPT4All":
             llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case _default:
@@ -69,6 +71,7 @@ def main():
         for document in docs:
             print("\n> " + document.metadata["source"] + ":")
             print(document.page_content)
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
